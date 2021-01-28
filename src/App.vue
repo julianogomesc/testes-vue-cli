74615fbb123dd8df6f1885d639d166cb7747da08
Juliano Gomes da Costa <63646218+julianogomesc@users.noreply.github.com>
2021-01-28 15:55:49 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    {{ name }}
+    <button @click="alertAdd()">Add</button>
+    <button @click="alertSub()">Sub</button>
+    <input type="text" :value="item" />
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: "teste",
+      item: 1,
+    };
+  },
+  methods: {
+    alertAdd() {
+      // this.age++;
+      this.item++;
+    },
+    alertSub() {
+      // this.age++;
+      this.item--;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
